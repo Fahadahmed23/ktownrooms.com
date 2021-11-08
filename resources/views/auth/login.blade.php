2a0f0af25412a3235884f30358f05879ff1f7809
Fahadahmed23 <fahadahmedoptimist@gmail.com>
2021-11-09 00:08:44 +0500

    asdsa

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,7 +10,7 @@
 	<link href="https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900" rel="stylesheet" type="text/css">
 	<link href="{{asset('global_assets/css/icons/icomoon/styles.min.css')}}" rel="stylesheet" type="text/css">
 	<!-- <link href="{{asset('assets/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css"> -->
-	<link href="{{assets('css/bootstrap.min.css')}}" rel="stylesheet" type="text/css">
+	<link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet" type="text/css">
 	
 	<link href="{{asset('assets/css/bootstrap_limitless.min.css')}}" rel="stylesheet" type="text/css">
 	<link href="{{asset('assets/css/layout.min.css')}}" rel="stylesheet" type="text/css">
