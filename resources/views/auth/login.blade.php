71564cf42e19a67a354bdd4ccdd9e3ba39a42fb5
Fahadahmed23 <fahadahmedoptimist@gmail.com>
2021-11-09 00:09:36 +0500

    sdsa

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,31 +6,30 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<title>Ktown Rooms | Login</title>
+
 	<!-- Global stylesheets -->
 	<link href="https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900" rel="stylesheet" type="text/css">
-	<link href="{{asset('global_assets/css/icons/icomoon/styles.min.css')}}" rel="stylesheet" type="text/css">
-	<!-- <link href="{{asset('assets/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css"> -->
-	<link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet" type="text/css">
-	
-	<link href="{{asset('assets/css/bootstrap_limitless.min.css')}}" rel="stylesheet" type="text/css">
-	<link href="{{asset('assets/css/layout.min.css')}}" rel="stylesheet" type="text/css">
-	<link href="{{asset('assets/css/components.min.css')}}" rel="stylesheet" type="text/css">
-	<link href="{{asset('assets/css/colors.min.css')}}" rel="stylesheet" type="text/css">
+	<link href="{{asset('public/global_assets/css/icons/icomoon/styles.min.css')}}" rel="stylesheet" type="text/css">
+	<link href="{{asset('public/assets/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css">
+	<link href="{{asset('public/assets/css/bootstrap_limitless.min.css')}}" rel="stylesheet" type="text/css">
+	<link href="{{asset('public/assets/css/layout.min.css')}}" rel="stylesheet" type="text/css">
+	<link href="{{asset('public/assets/css/components.min.css')}}" rel="stylesheet" type="text/css">
+	<link href="{{asset('public/assets/css/colors.min.css')}}" rel="stylesheet" type="text/css">
 	<!-- /global stylesheets -->
 
 	<!-- Core JS files -->
-	<script src="{{asset('global_assets/js/main/jquery.min.js')}}"></script>
-	<script src="{{asset('global_assets/js/main/bootstrap.bundle.min.js')}}"></script>
-	<script src="{{asset('global_assets/js/plugins/loaders/blockui.min.js')}}"></script>
-	<script src="{{asset('global_assets/js/plugins/ui/ripple.min.js')}}"></script>
+	<script src="{{asset('public/global_assets/js/main/jquery.min.js')}}"></script>
+	<script src="{{asset('public/global_assets/js/main/bootstrap.bundle.min.js')}}"></script>
+	<script src="{{asset('public/global_assets/js/plugins/loaders/blockui.min.js')}}"></script>
+	<script src="{{asset('public/global_assets/js/plugins/ui/ripple.min.js')}}"></script>
 	<script src="https://kit.fontawesome.com/ff383a412e.js" crossorigin="anonymous"></script>
 	<!-- /core JS files -->
 
 	<!-- Theme JS files -->
-	<script src="{{asset('global_assets/js/plugins/forms/styling/uniform.min.js')}}"></script>
+	<script src="{{asset('public/global_assets/js/plugins/forms/styling/uniform.min.js')}}"></script>
 
-	<script src="assets/js/app.js"></script>
-	<script src="{{asset('global_assets/js/demo_pages/login.js')}}"></script>
+	<script src="public/assets/js/app.js"></script>
+	<script src="{{asset('public/global_assets/js/demo_pages/login.js')}}"></script>
 	<!-- /theme JS files -->
 
 	<style>
