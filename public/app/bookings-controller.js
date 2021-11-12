e4f480252d313b2840f84f2b233f51e6e01326c9
Fahadahmed23 <fahadahmedoptimist@gmail.com>
2021-11-12 23:06:12 +0500

    Armaan

--- public/app/bookings-controller.js
+++ public/app/bookings-controller.js
@@ -3969,7 +3969,7 @@ app.controller('bookingsCtrl', function($scope, $rootScope, DTColumnDefBuilder, 
         $scope.nBooking.customer.Email = cus[0].Email;
         $('#showcustomerList').modal('hide');
     }
-    //comment
+    //comment yes yes
 
     //Get User Information from DB in User Contact Form of New Bookings - Arman Ahmad - End
 
