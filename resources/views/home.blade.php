fb41788aecc221795150b619883134ab8f6915b8
armanahmd <arman.ahmad@cloudprimero.com>
2021-11-22 21:15:12 +0500

    jk

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,42 +12,39 @@
             <div class="card">
                 @auth
                 <div class="card-header">Welcome {{ Auth::user()->name }}</div>
-
+[[customerbooking|json]]
                 <div class="card-body">
                 <div class="row">
                     <div class="col-md-12">
-[[customerbooking]]
+
                         <div class="table-responsive">
                             <table id="dTable" class="table table-user table-striped hover display datatable-basic" datatable="ng" dt-options="dtOptions" dt-column-defs="dtColumnDefs">
                                 <thead>
                                     <tr>
                                         <th>Client Name</th>
-                                        <th>Email</th>
+
                                         <th>Phone No.</th>
-                                        <th>Role</th>
-                                        <th>Hotel(s)</th>
-                                        <th>Registered at</th>
+                                        <th>Cnic</th>
+                                        <th>Email</th>
+                                        <th>Hotel Name</th>
+                                        <th>Booking Status</th>
+                                        <th>Booking Date</th>
                                         <th>Action</th>
                                     </tr>
                                 </thead>
                                 <tbody data-link="row" class="rowlink">
-                                    <tr ng-repeat="user in users" class="unread">
-                                        <td>[[user.name]]</td>
-                                        <td>[[user.email]]</td>
-                                        <td>[[user.phone_no]]</td>
-                                        <td>[[user.roles[0].name]]</td>
-                                        <td>[[user.HotelNames.join(", ")]]</td>
-                                        <td>[[user.created_at|date]]</td>
+                                    <tr ng-repeat="user in customerbooking.clients" class="unread">
+                                        <td>[[user.FullName]]</td>
+                                        <td>[[user.customer_last_name]]</td>
+                                        <td>[[user.customer_cnic]]</td>
+                                        <td>[[user.customer_email]]</td>
+                                        <td>[[user.customer_phone]]</td>
+                                        <td>Booking status</td>
+                                        <td>[[user.checkout_date|date]]</td>
                                         <td>
                                             <div class="align-self-center">
                                                 <div class="list-icons list-icons-extended">
-                                                    @permission('can-edit-user')
                                                     <a id="edit-company" ng-click="editUser(user)" class="list-icons-item text-info edit-info" data-popup="tooltip" title="Edit Detail" data-trigger="hover"><i class="icon-pencil5"></i></a>
-                                                    @endpermission
-                                                    @permission('can-delete-user')
-                                                    <a id="delete-company" ng-click="deleteUser(user)" class="list-icons-item text-danger delete"  data-popup="tooltip" title="Delete" data-trigger="hover"><i class="fas fa-trash"></i></a>
-                                                    @endpermission
-                                                    <a ng-click="resendPassword(user.id)" data-popup="tooltip" title="Reset Password" class="list-icons-item text-success"><i class="fas fa-envelope"></i></a>
                                                 </div>
                                             </div>
                                         </td>
