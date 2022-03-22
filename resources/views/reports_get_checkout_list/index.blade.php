b2c2cdb753f4e85657a35a4ff524e537c392312b
armanahmd <arman.ahmad@cloudprimero.com>
2022-03-22 23:45:57 +0500

    html view table

--- resources/views/reports_get_checkout_list/index.blade.php
+++ resources/views/reports_get_checkout_list/index.blade.php
@@ -19,6 +19,37 @@
                         <h4 class="card-title">Get CheckOut List</h4>
                     </div>
 
+                    <table class="table">
+                        <thead>
+                          <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">First</th>
+                            <th scope="col">Last</th>
+                            <th scope="col">Handle</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                          </tr>
+                          <tr>
+                            <th scope="row">2</th>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>@fat</td>
+                          </tr>
+                          <tr>
+                            <th scope="row">3</th>
+                            <td>Larry</td>
+                            <td>the Bird</td>
+                            <td>@twitter</td>
+                          </tr>
+                        </tbody>
+                      </table>
+
                     <div class="card-body">
                         {{-- [[modules]] --}}
                         <div style="display: inline-flex; width: 100%;" class="justify-content-lg-between">
--- resources/views/reports_get_guest_detail/index.blade.php
+++ resources/views/reports_get_guest_detail/index.blade.php
@@ -18,6 +18,36 @@
                     <div class="card-header header-elements-inline">
                         <h4 class="card-title">Get Guest Details</h4>
                     </div>
+                    <table class="table">
+                        <thead>
+                          <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">First</th>
+                            <th scope="col">Last</th>
+                            <th scope="col">Handle</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                          </tr>
+                          <tr>
+                            <th scope="row">2</th>
+                            <td ng-model="response.totalRecords">Jacob</td>
+                            <td>Thornton</td>
+                            <td>@fat</td>
+                          </tr>
+                          <tr>
+                            <th scope="row">3</th>
+                            <td>Larry</td>
+                            <td>the Bird</td>
+                            <td>@twitter</td>
+                          </tr>
+                        </tbody>
+                      </table>
 
                     <div class="card-body">
                         {{-- [[modules]] --}}
